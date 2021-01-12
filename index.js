fab3b5924d2ea77a45e857f5c4fb746eb8caabb7
hengshanMWC <1819293933@qq.com>
2021-01-12 16:16:17 +0800

    feat: 参数合并

--- index.js
+++ index.js
@@ -2,18 +2,20 @@
 /**
  * 多次调用，响应一次，可开缓存
  * 
- * @param {Function} fn 一个返回Promise的函数
- * @param {Boolean} cache 是否开启缓存
- * @param {*} context 上下文
- * @returns {Function} return Promise的闭包
+ * @param {string | Function} key 开启localStorage | 一个返回Promise的函数
+ * @param {Function | boolean} fn 一个返回Promise的函数 | 是否开启缓存
+ * @param {boolean | object} cache 是否开启缓存 | 上下文
+ * @param {object} context 上下文
+ * @returns {()=>Promise} return Promise的闭包
  */
-function oneHandle (fn, cache, context) {
+function oneHandle (key, fn, cache, context) {
   const queueThen = []
   const queueCatch = []
-  let cacheData
-  return function () {
+  const params = paramConcoct(key, fn , cache, context)
+  let cacheData = localStorage.getItem(params.key)
+  function result () {
     return new Promise((resolve, reject) => {
-      if (cacheData !== undefined) {
+      if (cacheData !== undefined && cacheData !== null) {
         resolve(cacheData)
       } else if (queueThen.length) {
         queueThen.push(resolve)
@@ -21,9 +23,9 @@ function oneHandle (fn, cache, context) {
       } else {
         queueThen.push(resolve)
         queueCatch.push(reject)
-        fn.apply(context, arguments)
+        params.fn.apply(params.context, arguments)
           .then(data => {
-            if (cache) cacheData = data
+            if (params.cache) cacheData = data
             queueThen.forEach(then => then(data))
             queueThen.length = 0
             queueCatch.length = 0
@@ -36,7 +38,27 @@ function oneHandle (fn, cache, context) {
       }
     })
   }
+  return result
 }
+function paramConcoct (key, fn , cache, context) {
+  let result
+  if (typeof key === 'function') {
+    result = {
+      fn: key,
+      cache: fn,
+      context: cache
+    }
+  } else {
+    result = {
+      key,
+      fn,
+      cache,
+      context,
+    }
+  }
+  return result
+}
+
 if ('undefined' !== typeof module) {
   module.exports = oneHandle
 }
